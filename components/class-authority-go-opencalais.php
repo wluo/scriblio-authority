d1e8707a889b556cd0f970217aced13e54bbbd62
Casey Bisson <casey.bisson@gmail.com>
2014-01-09 17:27:20 -0800

    better handling of term insert errors
    see https://github.com/GigaOM/legacy-pro/issues/2093

--- components/class-authority-go-opencalais.php
+++ components/class-authority-go-opencalais.php
@@ -24,9 +24,9 @@ class Authority_GO_OpenCalais
 					// attempt to create a proper term if none exists
 					$term_id = wp_insert_term( $v->name, $oc_config['mapping'][ $v->_type ] );
 
-					if ( ! isset( $term_id['term_id'] ) )
+					if ( ! is_array( $term_id ) || ! isset( $term_id['term_id'] ) )
 					{
-						error_log( __FILE__ . ':' . __LINE__ .' failed to insert term '. print_r( $term_id, TRUE ) .' referrer:' . $_SERVER['HTTP_REFERER'] );
+						trigger_error( __FILE__ . ':' . __LINE__ .' failed to insert term '. print_r( $term_id, TRUE ) .' referrer:' . $_SERVER['HTTP_REFERER'], E_USER_NOTICE );
 						continue;
 					}
 
